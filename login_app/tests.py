fc012aeced4586b43b500424407451d3cbbe31e5
343829084 <343829084@qq.com>
2017-10-26 01:30:06 -0700

    增加测试用例，方法是在工程目录下运行python manage.py test login_app

--- login_app/tests.py
+++ login_app/tests.py
@@ -1,6 +1,21 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.test import TestCase
+from django.test.client import Client
 
 # Create your tests here.
+
+class TestAccounti(TestCase):
+    def test_basic_addtion(self):
+        self.assertEqual(1+1,2)
+
+    def test_user_login(self):
+        c = Client()
+        response = c.get('/accounts/login')
+        self.assertEqual(response.status_code, 200)
+        response = c.get('/')
+        self.assertEqual(response.status_code, 302)
+
+
+
+
